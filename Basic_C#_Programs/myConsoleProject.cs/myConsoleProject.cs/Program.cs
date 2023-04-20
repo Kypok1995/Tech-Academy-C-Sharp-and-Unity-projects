d467eeb0d926928a8254585b293c1687fb1159da
Andrei <atrubaa@gmail.com>
2023-04-19 19:08:35 -0700

    Added initial C# programs

---
+++ Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace namespacemyConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your name is:" + name);
+            Console.Read();
+        }
+    }
+}
