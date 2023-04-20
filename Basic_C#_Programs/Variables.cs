f505750d76b1f9e33e0251299956f5fbe477263d
Andrei <atrubaa@gmail.com>
2023-04-20 19:45:31 +0000

    Added code for variables

---
+++ Basic_C#_Programs/Variables.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace VariablesAndDataType{
+
+class Program 
+{
+	static void Main()
+	{
+        // Console.WriteLine("What is your name?");
+        // string myName = Console.ReadLine();
+        // Console.WriteLine("Have a good day " + myName);
+
+        // Console.WriteLine("What is your favourite number?");
+        // string favouriteNumber = Console.ReadLine();
+        // int favouriteNumberInt = Convert.ToInt32(favouriteNumber);
+        // Console.WriteLine("Your favourite number is:" + favouriteNumberInt);
+
+        char questionMark = '\u2103'; //unicode for '?'. It still works as represent 1 unicode character
+        Console.WriteLine(questionMark);
+		
+	}
+
+}
+
+}
