9c88211f6e1edf4090c3bb75388d7a72c058dbbc
Andrei <atrubaa@gmail.com>
2023-04-22 15:59:55 -0700

    Add IncomeCompare Function

---
+++ Basic_C#_Programs/MathApplication1.1/MathApplication1.1/IncomeCompare.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+namespace MathApplication1._1
+{
+    class Income
+    {
+        void IncomeCompare()
+        {
+            Console.WriteLine("Welcome to Anonymous Income Comparison Program!");
+
+            //To calculate 1st person salary
+            Console.WriteLine("Person 1 details \nPlease enter your hourly rate");
+            string person1Rate = Console.ReadLine();
+            int person1RateInt = Convert.ToInt32(person1Rate);
+            Console.WriteLine("Enter ampunt of hours you work weekly!");
+            string person1Hours = Console.ReadLine();
+            int person1HoursInt = Convert.ToInt32(person1Hours);
+            double person1Salary = person1HoursInt * person1RateInt * 52;
+
+            //To calculate 2nd person salary
+            Console.WriteLine("Person 2 details \nPlease enter your hourly rate");
+            string person2Rate = Console.ReadLine();
+            int person2RateInt = Convert.ToInt32(person2Rate);
+            Console.WriteLine("Enter ampunt of hours you work weekly!");
+            string person2Hours = Console.ReadLine();
+            int person2HoursInt = Convert.ToInt32(person2Hours);
+            double person2Salary = person2HoursInt * person2RateInt * 52;
+
+            //To show annual salary of each person
+            Console.WriteLine("Person 1 annual gross salary is: " + person1Salary);
+            Console.WriteLine("Person 2 annual gross salary is: " + person2Salary);
+
+            //To compare salaries
+            bool biggerSalary = person1Salary > person2Salary;
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+
+            if (biggerSalary == true)
+            {
+                Console.WriteLine("Yes, person 1 makes more money");
+                Console.Read();
+            }
+
+            else
+            {
+                Console.WriteLine("No, person 2 makes more money");
+                Console.Read();
+            }
+
+
+
+        }
+
+        static void Main()
+        {
+            var compare = new Income();
+            compare.IncomeCompare();
+
+        }
+
+
+
+
+    }
+    }
+
--- Basic_C#_Programs/MathApplication1.1/MathApplication1.1/Program.cs
+++ Basic_C#_Programs/MathApplication1.1/MathApplication1.1/Program.cs
@@ -8,9 +8,9 @@ namespace MathApplication1._1
 {
     class Program
     {
-        static void Main(string[] args)
+     /*   static void Main(string[] args)
         {
-            /*int total = 5 + 1488;
+            *//*int total = 5 + 1488;
             int otherTotal = 10 + 15;
             int combined = total + otherTotal;
             Console.WriteLine(combined);
@@ -24,23 +24,63 @@ namespace MathApplication1._1
             Console.WriteLine(division);
             Console.ReadLine();*/
 
-            Console.WriteLine("Write any number and check is it even or odd?");
-            string xStr = Console.ReadLine();
-            int x = Convert.ToInt32(xStr);
-            int reminder = x % 2;
+            /*  Console.WriteLine("Write any number and check is it even or odd?");
+              string xStr = Console.ReadLine();
+              int x = Convert.ToInt32(xStr);
+              int reminder = x % 2;
 
-            if (reminder == 0)
+              if (reminder == 0)
+              {
+                  Console.WriteLine("Your number is even");
+                  Console.ReadLine();
+              }
+
+              else
+              {
+                  Console.WriteLine("Your number is odd");
+                  Console.ReadLine();
+              }*/
+
+            /*   bool trueOrFalse = 12 < 5;
+               Console.Write(trueOrFalse.ToString());
+               Console.ReadLine();*/
+
+            /*int roomTemp = 70;
+            int currentTemperature = 70;
+
+            bool isWarm = currentTemperature != roomTemp;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();*/
+
+            /*Console.WriteLine("Enter any digit you like");
+            string answer = Console.ReadLine();
+            double answerToInt = Convert.ToDouble(answer)  / 12.5;
+            Console.WriteLine("Your number devided on 12.5 is: " + answerToInt);
+            Console.ReadLine();*/
+
+            /*Console.WriteLine("Enter any digit you like!");
+            string answer = Console.ReadLine();
+            int answerInt = Convert.ToInt32(answer);
+
+            if (answerInt > 50)
             {
-                Console.WriteLine("Your number is even");
+                Console.WriteLine("Your number is greater than 50, such a good number!");
                 Console.ReadLine();
             }
 
             else
             {
-                Console.WriteLine("Your number is odd");
+                Console.WriteLine("Your number is really small");
                 Console.ReadLine();
-            }
-        }
+            }*//*
+
+            Console.WriteLine("Enter any number to check it's remainder with division over 7");
+            string answer = Console.ReadLine();
+            int answerInt = Convert.ToInt32(answer);
+            int remainder = answerInt % 7;
+            Console.WriteLine("Remainder is: " + remainder);
+            Console.Read();
+        }*/
 
             
 
