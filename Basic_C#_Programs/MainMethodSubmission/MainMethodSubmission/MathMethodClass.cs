82ad8240730cd8634fa9131e422a6fce082712c1
Andrei <atrubaa@gmail.com>
2023-05-03 23:03:27 -0700

    Add a method overload assigment

---
+++ Basic_C#_Programs/MainMethodSubmission/MainMethodSubmission/MathMethodClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class MathMethodClass
+    {
+
+        public int MathMethod(int number) //method for finding an area of circle (approximate as it return an integer) 
+        {
+            double radiusDouble = Math.Pow(number, 2);
+            int radiusSquared = Convert.ToInt32(radiusDouble);
+            double circleAreaDouble = radiusSquared * Math.PI;
+            int circleArea = Convert.ToInt32(circleAreaDouble);
+
+            return circleArea;
+        }
+
+        public decimal MathMethod (decimal number)
+        {
+            double numberDouble = Convert.ToDouble(number);
+            double resultDouble = Math.Sqrt(numberDouble);
+            decimal result = Convert.ToDecimal(resultDouble);
+
+            return result;
+        }
+
+        public int MathMethod(string numberString)
+        {
+            int number = Convert.ToInt32(numberString);
+            int result = 1;
+            while (number != 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+
+            return result;
+            
+        }
+    }
+
+
+    
+}
---
+++ Basic_C#_Programs/MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new MathMethodClass();
+            Console.WriteLine("This is a first method, it counts area of circle with circle radius as parameter: " +  test.MathMethod(5)); //take int as parameter, return int
+            Console.WriteLine("This is a second method, it returns a square root of decimal number: "+ test.MathMethod(25m)); //take decimal as parameter, return decimal
+            Console.WriteLine("This is third method, it returns a factorial of number: " + test.MathMethod("5")); // take string as parameter, return integer
+
+
+
+        }
+    }
+}
