09bc64c91824ba23e840be5f63d783b59f64b864
Andrei <atrubaa@gmail.com>
2023-05-04 13:53:49 -0700

    Write a code for method class submission assigment

---
+++ Basic_C#_Programs/MethodClassSubmissionAssigment/MethodClassSubmissionAssigment/MathMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssigment
+{
+    class MathMethod
+    {
+        public void UselessMethod(int mathNumber, int shownNumber)
+        {
+            mathNumber = Convert.ToInt32(Math.Pow(mathNumber, 2)); //do math operation with first parameter
+            Console.WriteLine("The second parameter of this method is: " + shownNumber); //display the second integer to the screen.
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MethodClassSubmissionAssigment/MethodClassSubmissionAssigment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MethodClassSubmissionAssigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new MathMethod();
+            test.UselessMethod(4, 15); //Call the method in the class, passing in two numbers.
+            test.UselessMethod(shownNumber:228, mathNumber:5); //Call the method in the class, specifying the parameters by name.
+        }
+    }
+}
