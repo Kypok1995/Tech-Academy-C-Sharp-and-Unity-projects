b61fb803216b1a0dc67d8a01709eb9fbbbafffa3
Andrei <atrubaa@gmail.com>
2023-05-06 12:36:49 -0700

    Make a code with all requirements

---
+++ Basic_C#_Programs/ClubSubmissionAssigment/ClubSubmissionAssigment/MathMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubSubmissionAssigment
+{
+    static class MathMethod
+    {
+        public static int test;
+        public static void DivideByTwo(int number) //Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2
+        {
+            Console.WriteLine("Your number " + number + " divided by two equals: " + number / 2);
+        }
+
+        public static void DivideByTwo(double number, out double result)//overload a method + out parameter
+        {
+            result = number / 2;
+            Console.WriteLine("Your number " + number + " divided by two equals: " + result);
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClubSubmissionAssigment/ClubSubmissionAssigment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClubSubmissionAssigment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("In this program you can divide your number by two.\nPlease enter the integer");
+            while (true)
+            {
+                try
+                {
+                    //var test = new MathMethod(); //In the Main() method, instantiate that class. Not actual for static class
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    //test.DivideByTwo(number); //Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two.
+
+                    MathMethod.DivideByTwo(number);//same method with static class
+
+                    Console.WriteLine("Here you can find overloaded method with out parameter. This method can divide number with floating point.\nPlease enter any number");
+                    double numberDouble = Convert.ToDouble(Console.ReadLine());
+                    double result;
+                    MathMethod.DivideByTwo(numberDouble, out result); //use an overloaded method with parameter in static class
+
+                    break;
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("You should enter integer!");
+                }
+
+                catch(OverflowException)
+                {
+                    Console.WriteLine("Your entered number is too big for integer.\nEnter number between -2,147,483,648 and 2,147,483,647");
+                }
+
+             
+            }
+
+            
+          
+            
+        }
+    }
+}
