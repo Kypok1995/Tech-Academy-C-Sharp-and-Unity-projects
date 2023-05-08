412487a055e1efc1dae4c2f52b395c5e29a067de
Andrei <atrubaa@gmail.com>
2023-05-07 17:00:10 -0700

    Added interface

--- Basic_C#_Programs/ClassInheritanceAssigment/ClassInheritanceAssigment/Employee.cs
+++ Basic_C#_Programs/ClassInheritanceAssigment/ClassInheritanceAssigment/Employee.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 
 namespace ClassInheritanceAssigment
 {
-    public class Employee: Person //Create another class called Employee and have it inherit from the Person class. Give the Employee class a property called Id and have it be of data type int.
+    public class Employee: Person, IQuittable //Create another class called Employee and have it inherit from the Person class. Give the Employee class a property called Id and have it be of data type int.
     {
         public int id { get; set; }
 
         public override void SayName() //here is implementation for abstact method from abstract class Person
         {
             Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public void Quit() //Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose
+        {
+            Console.WriteLine("All right, that's enough for me!! I, " + FirstName + " " + LastName + " , leaving this job at " + DateTime.Now);
         }
 
     }
--- Basic_C#_Programs/ClassInheritanceAssigment/ClassInheritanceAssigment/Program.cs
+++ Basic_C#_Programs/ClassInheritanceAssigment/ClassInheritanceAssigment/Program.cs
@@ -8,6 +8,9 @@ namespace ClassInheritanceAssigment
         {
             Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" }; //Inside of the Main method, instantiate and initialize an Employee object with a first name of “Sample” and a last name of “Student
             employee.SayName(); //Call the superclass method SayName() on the Employee object.
+
+            IQuittable employee2 = new Employee() {FirstName = "Andrei", LastName = "Trubitsyn" }; //create an object of type IQuittable and call the Quit() method on it
+            employee2.Quit();
         }
     }
 }
