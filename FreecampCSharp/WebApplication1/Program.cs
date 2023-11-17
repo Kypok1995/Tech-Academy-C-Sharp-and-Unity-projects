53015b6182f72868fda5e911afa46549eaaa3d9b
Andrei <atrubaa@gmail.com>
2023-11-17 14:50:55 -0800

    Add some classes for managing services of app

--- FreecampCSharp/WebApplication1/Program.cs
+++ FreecampCSharp/WebApplication1/Program.cs
@@ -3,4 +3,34 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.ConfigureMiddleware();
+app.RegisterEndpoints(); 
+
 app.Run();
+
+
+
+public static partial class ServiceInitializer// class to initialize a services
+{
+    public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
+
+public static partial class MiddlewareInitializer //class responsible for middleware initialization
+{
+    public static WebApplication ConfigureMiddleware(this WebApplication app)
+    {
+    }
+}
+
+
+public static partial class EndpointMapper //code for managing endpoints of application
+{
+    public static WebApplication RegisterEndpoints(this WebApplication app)
+    {
+        return app;
+    }
+}
+
