f5edaefe7b16f883c5840d5d0b171bc8f5e76b12
Andrei <atrubaa@gmail.com>
2023-11-27 22:00:10 -0800

    Add code for new challenge

---
+++ Challenges/SearchInsertPositionChallenge/Program.cs
@@ -0,0 +1,10 @@
+﻿using SearchInsertPositionChallenge;
+
+Solution solution = new Solution();
+
+int[] nums = { 1, 3, 5, 6 };
+int target = 5;
+
+Console.WriteLine(solution.SearchInsert(nums, target));
+
+
---
+++ Challenges/SearchInsertPositionChallenge/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInsertPositionChallenge
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int index = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    index = i;
+                    break;
+                }
+                else if (nums[i] > target)
+                {
+                    index = i;
+                    break;
+                }
+                else if (nums[i] < target)
+                {
+                    index = i + 1;
+                }
+            }
+            return index;
+        }
+    }
+}
