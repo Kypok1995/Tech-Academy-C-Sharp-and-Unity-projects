056cd662029744fd59edcfea4725d37492417dd4
Andrei <atrubaa@gmail.com>
2024-02-07 22:29:53 -0800

    Add more shop items styling

--- FreecampCSharp/GameShop/obj/Debug/net7.0/GameShop.AssemblyInfo.cs
+++ FreecampCSharp/GameShop/obj/Debug/net7.0/GameShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca39610cc6c4d16aa810810040da83bee013e62a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
