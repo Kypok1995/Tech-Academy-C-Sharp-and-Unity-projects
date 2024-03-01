0522578d01fa70be54fa4c21757ef3e76b38c748
Andrei <atrubaa@gmail.com>
2024-02-29 21:27:02 -0800

    Start working on navbar

--- FreecampCSharp/GameShop/obj/Debug/net7.0/GameShop.AssemblyInfo.cs
+++ FreecampCSharp/GameShop/obj/Debug/net7.0/GameShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50ab1bfc2fb3d40808c4e066f3e3b9e066149cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
