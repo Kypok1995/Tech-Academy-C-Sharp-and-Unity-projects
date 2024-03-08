2984f444352ba5ad1ee4f85eda7b9e7fd6046b45
Andrei <atrubaa@gmail.com>
2024-03-08 10:19:35 -0800

    Add navbar styling

--- FreecampCSharp/GameShop/obj/Debug/net7.0/GameShop.AssemblyInfo.cs
+++ FreecampCSharp/GameShop/obj/Debug/net7.0/GameShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575246c217389c9259c180cf98085934843d2185")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
